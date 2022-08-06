b9dff1827d4d94ba9ffc8e1aa49bbbd8a9d6b41e
bugi-bit <85178323+bugi-bit@users.noreply.github.com>
2022-08-06 11:57:37 +0700

    COMMENT

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -100,7 +100,7 @@ export const commentPost = (value, id) => async (dispatch) => {
   try {
     const { data } = await api.comment(value, id);
 
-    dispatch({ type: "COMMENT", payload: data });
+    dispatch({ type: COMMENT, payload: data });
     return data.comments;
   } catch (error) {
     console.log(error);
